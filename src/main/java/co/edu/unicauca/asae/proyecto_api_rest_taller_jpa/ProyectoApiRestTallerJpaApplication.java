42ca2731d7474c4d8b962e3e6c0623a131f80b1b
Juan Carlos Neuta <jneuta@unicauca.edu.co>
2024-09-27 10:16:47 -0500

    first commit

---
+++ src/main/java/co/edu/unicauca/asae/proyecto_api_rest_taller_jpa/ProyectoApiRestTallerJpaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.proyecto_api_rest_taller_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiRestTallerJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiRestTallerJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicauca/asae/proyecto_api_rest_taller_jpa/ProyectoApiRestTallerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.proyecto_api_rest_taller_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApiRestTallerJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
